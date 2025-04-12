52a84cb65f9d1bb40321ae965308fb0368d3512a
quangan010903@gmail.com <quangan010903@gmail.com>
2025-04-12 21:34:22 +0700

    setup dự án

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import ProductPage from "./pages/product.jsx";
+import UserPage from "./pages/user.jsx";
+import "./styles/global.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage/>
+  },
+  {
+    path: "/product",
+    element: <ProductPage/>
+  },
+  {
+    path: "/user",
+    element: <UserPage/>
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
